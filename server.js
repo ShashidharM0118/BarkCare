f77c263e82833e5f0c82e10e61890bf7789defd4
Shashidhar B M <112196062+ShashidharM0118@users.noreply.github.com>
2024-02-21 15:33:07 +0530

    basic setup

---
+++ server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static("public"));
+
+
+//logger
+app.use((req, res, next) => {
+    req.time = new Date(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, req.time);
+    next();
+});
+
+app.get("/api", async (req, res) => {
+    
+    res.render("index.ejs");
+});
+
+app.listen(8000, () => {
+    console.log("connected to db & listening on port", 8000);
+});
